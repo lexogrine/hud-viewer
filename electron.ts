a64aae3ed7c163ed181e42ea1316e7402910e8bf
Hubert Walczak <hubertwalczak8@gmail.com>
2021-01-05 15:21:42 +0100

    initial commit

---
+++ electron.ts
@@ -0,0 +1,58 @@
+/* eslint-disable no-console */
+import { app, BrowserWindow } from 'electron';
+import path from 'path';
+export const isDev = process.env.DEV === 'true';
+
+
+const createMainWindow = () => {
+	let win: BrowserWindow | null;
+
+	if (app) {
+		app.on('window-all-closed', app.quit);
+
+		app.on('before-quit', async () => {
+			if (!win) return;
+
+			win.removeAllListeners('close');
+			win.close();
+		});
+	}
+
+	win = new BrowserWindow({
+		height: 835,
+		show: false,
+		frame: isDev,
+		titleBarStyle: isDev ? 'hidden' : 'default',
+		//resizable: isDev,
+		title: 'Lexogrine HUD Reader',
+		icon: path.join(__dirname, 'assets/icon.png'),
+		webPreferences: {
+			backgroundThrottling: false,
+			devTools: isDev
+		},
+		minWidth: 775,
+		minHeight: 835,
+		width: 1010
+	});
+
+	win.once('ready-to-show', () => {
+		if (win) {
+			win.show();
+		}
+	});
+	// win.setMenu(null);
+	win.setMenuBarVisibility(!isDev);
+
+	win.loadURL(isDev ? "http://localhost:3000" : `file://${__dirname}/build/index.html`);
+	win.on('close', () => {
+		win = null;
+		app.quit();
+	});
+};
+
+const lock = app.requestSingleInstanceLock();
+if (!lock) {
+	app.quit();
+} else {
+	app.on('ready', createMainWindow);
+}
